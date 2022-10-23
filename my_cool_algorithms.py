64b3e8de5866d5d60915745e136cbd23f4f20df4
CalebThian <calebthian@gmail.com>
2022-10-23 16:47:42 +0800

    create sel_win for apriori_gen

--- my_cool_algorithms.py
+++ my_cool_algorithms.py
@@ -1,7 +1,40 @@
-def generate_L1(input_data,a):
-    itemset = generate_itemset(input_data)
-    minsup = int(a.min_sup*len(itemset.keys()))
+itemset = dict()
+
+def apriori_gen(L,minsup,k):
+    Lk = dict()
+    keys = list(L.keys())
+    keys.sort()
     
+    sel_win = []
+    for i in range(k):
+        sel_win.append(0)
+    print(sel_win)
+    while True:
+        sel_win = sw_inc(sel_win,len(keys))
+        print(sel_win)
+        if sel_win==-1:
+            return
+    C = Lk.keys()
+    
+    # Scan itemset
+
+def sw_inc(sel_win,maximum): #sel_win increment, maximum is the maximum number it can have
+    for i in range(len(sel_win)-1,-1,-1):
+        if sel_win[i] != maximum-(len(sel_win)-1-i): # example: [0 3 4] for maximum = 4, next should be [1,2,3] but not [0 4 ?]
+            sel_win[i] += 1
+            for j in range(i+1,len(sel_win)):
+                sel_win[j] = sel_win[j-1]+1
+            return sel_win
+    return -1
+
+def str2numbers(s):
+    strs = s.split()
+    numbers = []
+    for n in numbers:
+        numbers.append(int(n))
+    return numbers
+    
+def generate_L1(minsup):   
     L1 = dict()
     for Id,items in itemset.items():
         for item in items:
@@ -9,13 +42,10 @@ def generate_L1(input_data,a):
                 L1[item] += 1 
             else:
                 L1[item] = 1
-    print(len(L1))
     L1 = prune(L1,minsup)
-    print(len(L1))
     return L1
 
 def generate_itemset(input_data):
-    itemset = dict()
     for data in input_data:
         Id = data[0]
         item = data[2]
@@ -23,8 +53,7 @@ def generate_itemset(input_data):
             itemset[Id].append(item)
         else:
             itemset[Id] = [item]
-    return itemset
-    
+            
 def prune(itemset,minsup):
     delete = []
     for k,v in itemset.items():
@@ -36,7 +65,10 @@ def prune(itemset,minsup):
     return itemset
               
 def apriori(input_data, a):
-    L = generate_L1(input_data,a)
+    generate_itemset(input_data)
+    minsup = int(a.min_sup*len(itemset.keys()))
+    L = generate_L1(minsup)
     for k,v in L.items():
         print(k,v)
+    apriori_gen(L,minsup,3)
     
